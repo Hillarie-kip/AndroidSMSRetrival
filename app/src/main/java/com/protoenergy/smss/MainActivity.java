1a0e80842750753c7e837ef615d1bba56b5881fd
Hillarie-kip <kalyahillary@gmail.com>
2020-02-13 19:22:34 +0300

    initial commit

---
+++ app/src/main/java/com/protoenergy/smss/MainActivity.java
@@ -0,0 +1,118 @@
+package com.protoenergy.smss;
+
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity inst;
+    ArrayList<String> smsMessagesList = new ArrayList<String>();
+    ListView smsListView;
+    ArrayAdapter arrayAdapter;
+
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smsListView = (ListView) findViewById(R.id.SMSList);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsMessagesList);
+        smsListView.setAdapter(arrayAdapter);
+
+        if (ContextCompat.checkSelfPermission(getBaseContext(), "android.permission.READ_SMS") == PackageManager.PERMISSION_GRANTED) {
+
+            refreshSmsInbox();
+
+        } else {
+            final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{"android.permission.READ_SMS"}, REQUEST_CODE_ASK_PERMISSIONS);
+        }
+
+
+    }
+
+
+    public void refreshSmsInbox() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms"), null, null, null, null);
+        int idColumn = smsInboxCursor.getColumnIndex("_id");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int datemade = smsInboxCursor.getColumnIndex("date");
+
+
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            //if (smsInboxCursor.getString(indexBody).contains("pesa") || smsInboxCursor.getString(indexBody).contains("pro gas")) {
+                String str = "" + smsInboxCursor.getString(idColumn) + "\n" + "SMS From: " + smsInboxCursor.getString(indexAddress) + "\n" + smsInboxCursor.getString(datemade) + "\n" + "To Hillarie" + "\n" + smsInboxCursor.getString(indexBody) + "\n";
+                arrayAdapter.add(str);
+                saveMasterSaleToLocalStorage(str);
+
+           //   }
+
+
+        } while (smsInboxCursor.moveToNext());
+    }
+
+    private void saveMasterSaleToLocalStorage(String str) {
+     /*   db.addOpeningBalance(str);
+      _Content n = new _Content( str);
+        openingBalancecontent.add(n);*/
+    }
+
+
+    public void updateList(final String smsMessage) {
+        arrayAdapter.insert(smsMessage, 0);
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+        try {
+            String[] smsMessages = smsMessagesList.get(pos).split("\n");
+            String address = smsMessages[0];
+            String smsMessage = "";
+            for (int i = 1; i < smsMessages.length; ++i) {
+                smsMessage += smsMessages[i];
+            }
+
+            String smsMessageStr = address + "\n";
+            smsMessageStr += smsMessage;
+            Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
